1b3c7dd16f07837a34e6eb862cbbee8f55d24269
cpfarher <c.pfarher@gmail.com>
2011-04-27 21:13:52 -0300

    exercises finish chapter4

---
+++ app/helpers/exercise1.rb
@@ -0,0 +1,4 @@
+#4.5 exercise1
+def string_shuffle(s)
+	s.split('').shuffle.join
+end
---
+++ app/helpers/exercise3.rb
@@ -0,0 +1,6 @@
+person1 = { :firstname => 'person1 firstname', :lastname => 'lastname person 1' }
+person2 = { :firstname => 'person2 firstname', :lastname => 'lastname person 2' }
+person3 = { :firstname => 'person3 firstname', :lastanem => 'lastname person 3' }
+params = { :father => person1 }
+params = { :mother => person2 }
+params = { :child => person3 }
