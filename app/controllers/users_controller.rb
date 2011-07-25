053904886772737c928ce8cde240e0d2408bd512
cpfarher <c.pfarher@gmail.com>
2011-07-25 19:08:06 -0300

    8.2.4 Filtering parameter logging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   def new
+		@user = User.new
     @title = "Sign up"
   end
   
+  def create
+    @user = User.new(params[:user])
+    if @user.save 
+			## Handle a successful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
   def show
-	@user=User.find(params[:id])
-  @title = @user.name
+		@user=User.find(params[:id])
+		@title = @user.name
   end
 end
