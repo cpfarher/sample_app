0886a7debeb26ca42743079063055a9883c1978d
cpfarher <c.pfarher@gmail.com>
2011-07-25 19:40:02 -0300

    hasta 8.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(params[:user])
-    if @user.save 
+    if @user.save
+			flash[:success] = "Welcome to the Sample App!"	
 			redirect_to user_path(@user)
     else
       @title = "Sign up"
