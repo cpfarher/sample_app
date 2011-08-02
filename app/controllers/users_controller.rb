9ce6eea674847e90c5dcc6738383597871bee7ea
cpfarher <c.pfarher@gmail.com>
2011-08-01 23:17:40 -0300

    Done with sign in - section 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+			sign_in @user
 			flash[:success] = "Welcome to the Sample App!"	
-			redirect_to user_path(@user)
+			redirect_to @user
     else
       @title = "Sign up"
       render 'new'
