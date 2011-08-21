909b3f1abe45139507b9b3cc9e08f86d7dcf824b
esteban <efornal@gmail.com>
2011-08-21 18:57:55 -0300

    hasta 11.3.2 Creating microposts, sin incluír

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,4 +24,13 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def authenticate
+    deny_access unless signed_in?
+  end
+
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ SampleApp::Application.routes.draw do
   get "sessions/new"
 
   resources :users
-	resources :sessions, :only => [:new, :create, :destroy]
+  resources :sessions, :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
+
   #get "users/new"
-	match '/signup',  :to => 'users#new'
+  match '/signup',  :to => 'users#new'
   match '/signin',  :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
-	# get "pages/home"
-	# get "pages/contact"
-	# get "pages/about"
+  # get "pages/home"
+  # get "pages/contact"
+  # get "pages/about"
   root              :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
