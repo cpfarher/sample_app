56e98050e1cab956152835ccb97a4f641c055ec4
cpfarher <c.pfarher@gmail.com>
2011-08-30 18:24:18 -0300

    hasta 12.1.5 Followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,8 @@ class User < ActiveRecord::Base
   has_many :microposts, :dependent => :destroy
   has_many :relationships, :foreign_key => "follower_id",
                            :dependent => :destroy
-  
+  has_many :following, :through => :relationships, :source => :followed
+
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :name,  :presence => true,
@@ -52,6 +53,18 @@ class User < ActiveRecord::Base
     # This is preliminary. See Chapter 12 for the full implementation.
     Micropost.where("user_id = ?", id)
   end
+
+	def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
 	
 	private
 
@@ -71,5 +84,6 @@ class User < ActiveRecord::Base
 	def secure_hash(string)
 		Digest::SHA2.hexdigest(string)
 	end
-	
+
+  
 end
